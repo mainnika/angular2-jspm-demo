c149da8c35395ef04aa1f4ae5e736eaed91e820a
mainnika <nikita@tokarch.uk>
2016-06-13 05:31:53 +0500

    compatible with jspm bundle

--- lib/app.ts
+++ lib/app.ts
@@ -1,3 +1,8 @@
+import 'core.js';
+import 'zone.js';
+import 'reflect-metadata';
+
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+
 bootstrap(AppComponent);
--- lib/starter.ts
+++ lib/starter.ts
@@ -1,15 +1,3 @@
-// System.import('core.js');
-
-System.map['crypto'] = '@empty';
-
-System.import('core-js');
-System.import('zone.js');
-System.import('reflect-metadata');
-
 System.import('app')
-    .then(() => {
-        console.log('App started');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+    .then(() => console.log('App started'))
+    .catch((err) => console.error(err))
